91fec44ba754d352926bf6bb4fdf4c5ab3def387
Lorenzo-Testa <testalorenzo99@gmail.com>
2022-10-07 12:02:21 +0200

    Aggiunta commenti per completamento checklist

--- 2-CodeVerification/Progetto_Testa_Verification/src/progettosemaforo/Semaforo.java
+++ 2-CodeVerification/Progetto_Testa_Verification/src/progettosemaforo/Semaforo.java
@@ -1,19 +1,24 @@
 package progettosemaforo;
 public class Semaforo {
 
+	//stato indica il colore attuale dei due semafori
 	private /*@ spec_public @*/ int[] stato;
 	
 	//@ public invariant (\exists int i; i>0 && i<stato.length; stato[i] == 2);
 	
+	//costruttore: inizializzo entrambi i semafori a rossi
 	//@ ensures stato != null;
 	//@ ensures (\forall int i; i>0 && i<stato.length; stato[i] == 2);
 	public Semaforo() {
-		// nel costruttore li inizializzo entrambi rossi
 		stato = new int[2];
 		stato[0] = 2;
 		stato[1] = 2;
 	}
 
+	//funzione changecolor: dato un intero per il colore e uno per la selezione del semaforo
+	//permette di cambiare il colore del semaforo selezionato sul colore scelto, solo se
+	//e' nella sequenza verde, giallo, rosso e da rosso a verde solo se l'altro semaforo
+	//e' a rosso.
 	//@requires sem >= 0;
 	//@requires sem <= 2;
 	//@requires color >=0;
@@ -54,6 +59,7 @@ public class Semaforo {
 		}
 	}
 
+	//funzione toString: permette di restituire tramite due char il colore dei semafori al momento in cui viene chiamata
 	//@ also
 	//@ ensures (\forall int i; i>0 && i<stato.length; stato[i] == \old(stato[i]));
 	@Override
